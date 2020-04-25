b5b2eaf8dc9859a2dce93d531d7339ea7914d8e3
Prashul Jain <jainprashul@gmail.com>
2020-04-25 21:59:39 +0530

    travis file incoming

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-  // Reait('renders without crashing', () => {
+it('renders without crashing', () => {
   // const div = document.createElement('div');
-ctDOM.render(<App />, div);
+  // ReactDOM.render(<App />, div);
   // ReactDOM.unmountComponentAtNode(div);
 });
